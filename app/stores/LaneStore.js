9bb2ad132ea9ab16de69c62928586c1550d79e15
Anthony Ng <newyork.anthonyng@gmail.com>
2016-07-08 07:42:19 -0400

    Handle data dependencies

---
+++ app/stores/LaneStore.js
@@ -0,0 +1,46 @@
+import LaneActions from '../actions/LaneActions';
+
+export default class LaneStore {
+	constructor() {
+		this.bindActions(LaneActions);
+		this.lanes = [];
+	}
+
+	create(lane) {
+		lane.notes = lane.notes || [];
+
+		this.setState({
+			lanes: this.lanes.concat(lane)
+		});
+	}
+
+	attachToLane({laneId, noteId}) {
+		const newLanes = this.lanes.map(lane => {
+			// remove the note from any existing lanes
+			if(lane.notes.includes(noteId)) {
+				lane.notes = lane.notes.filter(note => note !== noteId);
+			}
+
+			// add the note to target lane
+			if(lane.id === laneId) {
+				lane.notes = lane.notes.concat([noteId]);
+			}
+
+			return lane;
+		});
+
+		this.setState({lanes: newLanes});
+	}
+
+	detachFromLane({laneId, noteId}) {
+		const newLanes = this.lanes.map(lane => {
+			if(lane.id === laneId) {
+				lane.notes = lane.notes.filter(note => note.id !== noteId);
+			}
+
+			return lane;
+		});
+
+		this.setState({lanes: newLanes});
+	}
+}
